1ab668592ab9edbf6853acdbfa7d335237573bac
gabrielfior <gabriel.fior@gobrsa.org>
2018-09-24 16:41:26 +0200

    updated n_rows

--- dash-app.py
+++ dash-app.py
@@ -26,7 +26,7 @@ DF_SIMPLE = pd.DataFrame({
     'z': ['a', 'b', 'c', 'a', 'b', 'c']
 })
 
-n_rows = 1000000
+n_rows = 10000
 list_excel_cols = list(itertools.islice(excel_cols(), n_rows))
 list_excel_cols_shuffled = list(itertools.islice(excel_cols(), n_rows))
 shuffle(list_excel_cols_shuffled)
